71baec7c1480639d2e937b948399575a34073127
jungeleon <leon.junge@me.com>
2025-07-28 23:10:30 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ NOTIFY_CHAT_ID = os.environ["NOTIFY_CHAT_ID"]
 KEYWORDS = ["Wipkingen", "8037", "8057", "8005", "Kreis 10", "Kreis 6", "Kreis 5", "K10", "K6", "K5"]
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print("Nachricht empfangen:", update.message.text)
     if update.message and update.message.text:
         message_text = update.message.text.lower()
         if any(keyword in message_text for keyword in KEYWORDS):
